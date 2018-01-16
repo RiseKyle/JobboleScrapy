a1c208d3dc60a6517569ccf8f9c783913ee3de8d
RiseKyle <kyle.zxy@qq.com>
2018-01-16 09:53:54 +0800

    Initial commit

---
+++ JobboleScrapy/main.py
@@ -0,0 +1,10 @@
+from scrapy.cmdline import execute
+import sys
+import os
+
+# 打印main.py的路径
+print(os.path.abspath(__file__))
+# 通过main.py获取项目路径
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# 执行scrapy脚本
+execute(["scrapy","crawl","jobbole"])
